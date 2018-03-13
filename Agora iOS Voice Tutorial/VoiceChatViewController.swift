976a768601792be19428646b435f1d2b43807e56
yiming <672739278@qq.com>
2018-03-13 22:05:33 +0800

    modify int uid to string uid for Agora-iOS-Voice-Tutorial-Swift-1to1_StringUID

---
+++ Agora iOS Voice Tutorial/VoiceChatViewController.swift
@@ -0,0 +1,66 @@
+//
+//  VoiceChatViewController.swift
+//  Agora iOS Voice Tutorial
+//
+//  Created by GongYuhua on 2017/4/10.
+//  Copyright © 2017年 Agora. All rights reserved.
+//
+
+import UIKit
+
+class VoiceChatViewController: UIViewController {
+
+    @IBOutlet weak var controlButtonsView: UIView!
+    
+    var agoraKit: AgoraRtcEngineKit!
+    let localUid : String = {
+        let now = Date()
+        let timeInterval:TimeInterval = now.timeIntervalSince1970
+        print("当前时间戳：\(timeInterval)")
+        return String(timeInterval)
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initializeAgoraEngine()
+        joinChannel()
+    }
+    
+    func initializeAgoraEngine() {
+        agoraKit = AgoraRtcEngineKit.sharedEngine(withAppId: AppID, delegate: nil)
+    }
+    
+    func joinChannel() {
+        agoraKit.joinChannel(byKey: nil, channelName: "voiceDemoChannel1", info:nil, uid:localUid) {[unowned self] (sid, uid, elapsed) -> Void in
+            // Joined channel "demoChannel"
+            self.agoraKit.setEnableSpeakerphone(true)
+            UIApplication.shared.isIdleTimerDisabled = true
+        }
+    }
+    
+    @IBAction func didClickHangUpButton(_ sender: UIButton) {
+        leaveChannel()
+    }
+    
+    func leaveChannel() {
+        agoraKit.leaveChannel(nil)
+        hideControlButtons()
+        
+        UIApplication.shared.isIdleTimerDisabled = false
+    }
+    
+    func hideControlButtons() {
+        controlButtonsView.isHidden = true
+    }
+    
+    @IBAction func didClickMuteButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        agoraKit.muteLocalAudioStream(sender.isSelected)
+    }
+    
+    @IBAction func didClickSwitchSpeakerButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        agoraKit.setEnableSpeakerphone(sender.isSelected)
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+#modify refer to Agora iOS Voice Tutorial for Swift - 1to1
+https://github.com/AgoraIO/Agora-iOS-Voice-Tutorial-Swift-1to1
